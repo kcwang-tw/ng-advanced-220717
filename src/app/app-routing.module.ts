720f7efafc95040c397532f6f8fe01371d7170e9
kcwang <kcwang.work@gmail.com>
2022-07-17 10:58:23 +0800

    新增路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import { ColorsComponent } from './utilities/colors/colors.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
+  {
+    path: 'utilities',
+    children: [
+      { path: 'colors', component: ColorsComponent }
+    ]
+  },
 ];
 
 @NgModule({
